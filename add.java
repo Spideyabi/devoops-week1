2239b7ebd6dc99a75d2a09daf4f263d2e0a9a1e2
sirichanda <sirichanda08@gmail.com>
2026-01-09 10:15:39 +0530

    this is jaa file

---
+++ add.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class add {
+public static void main(String args[])
+{
+system.out.println("this is Java program);
+int a=10,b=20,c;
+c=a+b;
+system.out.println("sum is "+c);
+}
+}
